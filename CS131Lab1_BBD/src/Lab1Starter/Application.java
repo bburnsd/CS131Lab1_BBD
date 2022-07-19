1a6dd65c89e32c880d0f737544c38460e80613da
bbdav <bbdav@DESKTOP-NP1DMG1>
2022-07-19 08:47:40 -0400

    Commit

---
+++ CS131Lab1_BBD/src/Lab1Starter/Application.java
@@ -0,0 +1,25 @@
+package Lab1Starter;
+/**
+ * This is the Application object that will launch our application
+ * @author Rob Kelley
+ * @version 1.3
+ * Lab1 
+ * CS131ON
+ */
+public class Application {
+
+	public static void main(String[] args) {
+		
+		NonPlayerCharacter npc = new NonPlayerCharacter();
+		GameCharacter pc = new GameCharacter();
+		
+		
+		
+		//Uncomment the line below to ensure reportStructure() method works after you have written it.
+		System.out.println(npc.reportStructure());
+		System.out.println(npc.introduce());
+		System.out.println(npc.exclaim());
+
+	}//end main
+
+}//end class
